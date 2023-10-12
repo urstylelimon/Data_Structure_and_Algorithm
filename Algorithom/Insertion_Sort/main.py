78474e75e4f96d7864461196592d8703aa499a67
urstylelimon <mhlimon571@gmail.com>
2023-10-13 01:38:17 +0600

    insertion_sort algorithm implement

---
+++ Algorithom/Insertion_Sort/main.py
@@ -0,0 +1,14 @@
+nums = [6,2,5,1,3,4]
+
+for i in range(1,len(nums)):
+    j = i
+    while i > 0:
+        if nums[i-1] > nums[j]:
+            temp = nums[i-1]
+            nums[i-1] = nums[j]
+            nums[j] = temp
+            j = i-1
+        i -= 1
+
+print(nums)
+
