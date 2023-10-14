7aa54aa5584e0391109275b2607b48f12c19494a
urstylelimon <mhlimon571@gmail.com>
2023-10-14 21:57:30 +0600

    implement bubble_sort algorithm

---
+++ Algorithom/Bubble_Sort/main.py
@@ -0,0 +1,13 @@
+nums = [6,2,5,1,3,4]
+n = 1
+for i in range(len(nums)):
+    j = 0
+    while j < len(nums)-n:
+        if nums[j] > nums[j+1]:
+            temp = nums[j]
+            nums[j] = nums[j+1]
+            nums[j+1] = temp
+        j += 1
+    n += 1
+
+print(nums)
