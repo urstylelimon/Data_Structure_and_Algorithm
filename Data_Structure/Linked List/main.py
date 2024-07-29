99186775f3a48633e91c6aa71e5cb874658b1b19
limon <mhlimon571@gmail.com>
2024-07-29 23:22:18 +0600

    print all linked list

--- Data_Structure/Linked List/main.py
+++ Data_Structure/Linked List/main.py
@@ -6,3 +6,14 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
+
+    def print_LL(self):
+        if self.head is None:
+            print("Linked List is empty")
+        else:
+            n = self.head
+            while n is not None:
+                print(n.value)
+                n = n.ref
+l1 = LinkedList()
+print(l1.print_LL())
