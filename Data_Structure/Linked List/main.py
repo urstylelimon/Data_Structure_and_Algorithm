b913a7f84f81062d2f43ecf96a8a9dfe14a338de
limon <mhlimon571@gmail.com>
2024-08-13 01:28:16 +0600

    Remove node based on value

--- Data_Structure/Linked List/main.py
+++ Data_Structure/Linked List/main.py
@@ -2,7 +2,6 @@ class Node():
     def __init__(self,data):
         self.data = data
         self.next = None
-        print(self.next)
 
 class linklist():
     def __init__(self):
@@ -16,12 +15,42 @@ class linklist():
             current = self.head
             while current.next is not None:
                 current = current.next
+            current.next = new_node
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data)
+            current = current.next
 
-
+    def delete_head(self):
+        if self.head is not None:
+            self.head = self.head.next
+    def delete_node(self,data):
+        if self.head is not None:
+            if self.head.data == data:
+                self.head = self.head.next
+            else:
+                current = self.head
+                while current:
+                    if current.next.data == data:
+                        current.next = current.next.next
+                        print("successfully Removed")
+                        break
+                    current = current.next
 
 
 
 
 l1 = linklist()
 l1.append(1)
+l1.append(2)
+l1.append(3)
+l1.append(4)
+
+
+
+print("Delete node")
+
+l1.delete_node(4)
+
 l1.display()
