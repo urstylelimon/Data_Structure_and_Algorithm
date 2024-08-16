576a5bfa258169db07a8f4b4d79fa0f286f1bbec
limon <mhlimon571@gmail.com>
2024-08-17 00:58:28 +0600

    doubly linklist insert and display

--- Data_Structure/Linked List/doubly_Linked_list.py
+++ Data_Structure/Linked List/doubly_Linked_list.py
@@ -1,67 +1,38 @@
-class Node:
-    def __init__(self, data):
+class Node():
+    def __init__(self,data):
         self.data = data
         self.next = None
         self.prev = None
-
-class DoublyLinkedList:
+class DoubleLinkedList():
     def __init__(self):
         self.head = None
 
-    def append(self, data):
+    def insert(self,data):
         new_node = Node(data)
+
         if self.head is None:
             self.head = new_node
-            return
-        last = self.head
-        while last.next:
-            last = last.next
-        last.next = new_node
-        new_node.prev = last
-
-    def prepend(self, data):
-        new_node = Node(data)
-        if self.head is None:
-            self.head = new_node
-            return
-        self.head.prev = new_node
-        new_node.next = self.head
-        self.head = new_node
-
-    def delete(self, node):
-        if self.head is None or node is None:
-            return
-
-        if node == self.head:
-            self.head = node.next
-
-        if node.next is not None:
-            node.next.prev = node.prev
-
-        if node.prev is not None:
-            node.prev.next = node.next
+        else:
+            current = self.head
+            while current.next is not None:
+                current = current.next
+            current.next = new_node
+            new_node.prev = current
 
     def display(self):
         current = self.head
         while current:
-            print(current.data, end=' ')
+            print(current.data)
+            if current.prev is None:
+                print(' -> None')
+            else:
+                print("Previos Value : ", current.prev.data)
             current = current.next
-        print()
 
-    def reverse_display(self):
-        current = self.head
-        while current and current.next:
-            current = current.next
-        while current:
-            print(current.data, end=' ')
-            current = current.prev
-        print()
+l1 = DoubleLinkedList()
+l1.insert(1)
+l1.insert(2)
+l1.insert(3)
+l1.insert(4)
 
-# Example usage:
-dll = DoublyLinkedList()
-dll.append(10)
-dll.append(20)
-dll.append(30)
-dll.prepend(5)
-dll.display()
-dll.reverse_display()
+l1.display()
