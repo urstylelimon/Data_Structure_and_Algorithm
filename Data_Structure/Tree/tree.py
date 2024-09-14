08d84f81992ee67b6de7a050e6626d76588a7f50
limon <mhlimon571@gmail.com>
2024-09-15 00:35:36 +0600

    tree data structure

---
+++ Data_Structure/Tree/tree.py
@@ -0,0 +1,9 @@
+class Node:
+    def __init__(self, data):
+        self.right = None
+        self.left = None
+        self.value = data
+
+class Tree:
+    def __init__(self):
+        self.root = None
