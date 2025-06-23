e676489ee866c74a3e085b8fac3d04710a4d6cbd
mhlim <mhlimon571@gmail.com>
2025-06-23 15:25:03 +0600

    implement tree

--- Data_Structure/Tree/tree.py
+++ Data_Structure/Tree/tree.py
@@ -1,9 +1,24 @@
 class Node:
     def __init__(self, data):
+        self.data = data
         self.right = None
         self.left = None
-        self.value = data
 
-class Tree:
-    def __init__(self):
-        self.root = None
+# Create tree nodes
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+
+#Traversal
+
+def inorder(node):
+    if node:
+        inorder(node.left)
+        print(node.data)
+        inorder(node.right)
+
+print("Inorder Traversal:")
+inorder(root)
