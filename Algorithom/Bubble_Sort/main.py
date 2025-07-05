57fbb4d81cf878c04ef07b7ea505b3e413c1fedd
limon <mhlimon571@gmail.com>
2025-07-06 01:30:48 +0600

    Implement Bubble sort with new code

--- Algorithom/Bubble_Sort/main.py
+++ Algorithom/Bubble_Sort/main.py
@@ -1,13 +1,13 @@
 nums = [6,2,5,1,1,3,4]
-n = 1
-for i in range(len(nums)):
-    j = 0
-    while j < len(nums)-n:
-        if nums[j] > nums[j+1]:
-            temp = nums[j]
-            nums[j] = nums[j+1]
-            nums[j+1] = temp
-        j += 1
-    n += 1
+
+n = len(nums)
+flag = 1
+while flag < n:
+    for i in range(len(nums)):
+        for k in range(i+1, len(nums)):
+            if nums[i] > nums[k]:
+                nums[i],nums[k] = nums[k],nums[i]
+                break
+    flag += 1
 
 print(nums)
